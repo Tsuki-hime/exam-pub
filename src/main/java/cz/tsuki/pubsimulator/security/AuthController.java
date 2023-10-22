68ed20dc5615a383cac78797f5620ac46f09a77e
Tsuki-hime <124776600+Tsuki-hime@users.noreply.github.com>
2023-10-22 13:33:14 +0200

    WIP: AuthController

---
+++ src/main/java/cz/tsuki/pubsimulator/security/AuthController.java
@@ -0,0 +1,29 @@
+package cz.tsuki.pubsimulator.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    // userService or create auth service?
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(){
+
+
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(){
+
+
+
+    }
+
+}
